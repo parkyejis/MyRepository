8e6aac3837645c72ef459380f1c54b0dcc125e2d
parkyejis <ontte0322@skuniv.ac.kr>
2025-12-14 21:49:36 +0900

    Changed classes B and C in Bob's branch

--- GitHW/src/githw/B.java
+++ GitHW/src/githw/B.java
@@ -4,5 +4,6 @@ public class B {
 
 	public B() {
 		System.out.println("B : master branch에서 변경");
+		System.out.println("Bob changed B class");
 	}
 }
--- GitHW/src/githw/C.java
+++ GitHW/src/githw/C.java
@@ -5,6 +5,7 @@ public class C {
 	public static void main(String[] args) {
 		System.out.println("C : master branch에서 생성");
 		System.out.println("master branch에서 변경");
+		System.out.println("Bob changed C class");
 	}
 
 }
